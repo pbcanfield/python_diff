d9558e7d8e7dd6393af496fb1dab42d11078be2b
Pete Canfield <mistermcgreen@gmail.com>
2021-05-07 14:08:00 -0500

    Fixed some issues

--- diff.py
+++ diff.py
@@ -10,7 +10,7 @@ def compare_lists(old, new):
 
     for item,token in enumerate(old):
         try:
-            offset_index = new.index(token)
+            offset_index = new.index(token ,item, len(new))
         except ValueError:
             removed[item] = token
         else:
@@ -34,10 +34,10 @@ def display_differences(i_dict, r_dict):
     else:
         print("File diffs: ")
         for line_num in i_dict:
-            print('\x1b[6;30;42m+%d %s \x1b[0m' % (line_num, i_dict[line_num]))
+            print('\x1b[6;30;42m+%d    %s \x1b[0m' % (line_num, i_dict[line_num]))
         
         for line_num in r_dict:
-            print('\x1b[0;30;41m-%d %s \x1b[0m' % (line_num, r_dict[line_num]))
+            print('\x1b[0;30;41m-%d    %s \x1b[0m' % (line_num, r_dict[line_num]))
 
 
 def main(o_file, n_file):
@@ -57,13 +57,11 @@ if __name__ == '__main__':
     arg_one = os.sys.argv[1]
     
     if arg_one == '--help':
-        print("Welcome to the file diff checker!")
-        print("Usage is simple, just enter the original")
-        print("file and the file you want to compare it")
-        print("to and see the differences.")
+        print("Welcome to the file diff checker! Usage is simple, just enter the original ", end = '')
+        print("file and the file you want to compare it to and see the differences.")
     else:
-        file_one = os.sys.argv[1]
-        file_two = os.sys.argv[2]
+        file_one = os.sys.argv[2]
+        file_two = os.sys.argv[1]
 
         #Arg checking.
         main(file_one, file_two)
